c805aa10f8fff057f718737e718f718a6253a529
younjs <younjs@uniquegood.biz>
2023-09-25 15:35:48 +0900

    Flutter 3.10.X 대응 추가

--- lib/src/widgets/raw_editor.dart
+++ lib/src/widgets/raw_editor.dart
@@ -350,7 +350,6 @@ class RawEditorState extends EditorState
   List<ContextMenuButtonItem> get contextMenuButtonItems {
     return EditableText.getEditableButtonItems(
       clipboardStatus: _clipboardStatus.value,
-      onLiveTextInput: null,
       onCopy: copyEnabled
           ? () => copySelection(SelectionChangedCause.toolbar)
           : null,
